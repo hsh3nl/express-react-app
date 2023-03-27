ab39ed0edc7583a269e8f4aafdbfd5a6f1fcad54
Aaron <hunshen.lau@gmail.com>
2023-03-27 23:49:30 +0800

    Added: context props interface

---
+++ src/web/src/shared/interfaces/context/context-props.interface.ts
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export interface ContextProps {
+    state: Record<string, any>;
+    actions: Record<string, any>;
+    methods: Record<string, any>;
+}
