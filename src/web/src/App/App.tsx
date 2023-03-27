52fa3367fc281df921d63e50c399c0f63fffda5a
Aaron <hunshen.lau@gmail.com>
2023-03-28 00:03:18 +0800

    Updated: app components

--- src/web/src/App/App.tsx
+++ src/web/src/App/App.tsx
@@ -1,10 +1,37 @@
+import { Navigate, Outlet, Route, Routes } from 'react-router-dom';
 import './App.css';
+
+// guards
+import AuthGuard from '../shared/guards/Auth/Auth.guard';
+
+// compose
+import ComponentCompose from '../shared/composes/ComponentCompose/ComponentCompose';
+
+// providers
+import ProfileProvider from '../shared/provider-context-hook/profile/Profile.provider';
+
+// components
+import HomeComponent from './Home/Home.component';
+import ProfileComponent from './Profile/Profile.component';
 
 function App(): JSX.Element {
     return (
-        <div>
-			<h1>Hello</h1>
-		</div>
+        <Routes>
+            <Route path="" element={<HomeComponent />} />
+            <Route
+                element={
+                    <AuthGuard>
+                        <ComponentCompose components={[ProfileProvider]}>
+                            <Outlet />
+                        </ComponentCompose>
+                    </AuthGuard>
+                }
+            >
+                <Route path="profile" element={<ProfileComponent />} />
+                <Route path="*" element={<Navigate to="/" replace />} />
+            </Route>
+            <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
     );
 }
 
